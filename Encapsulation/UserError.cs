be644ede82390a145e191cbe1f3b47a7deed962c
johanneswf <johanneswf@gmail.com>
2022-05-23 11:05:32 +0200

    Formatting.

--- Encapsulation/UserError.cs
+++ Encapsulation/UserError.cs
@@ -13,41 +13,26 @@ namespace Encapsulation
 
     public class NumericInputError : UserError
     {
-        public override string UEMessage()
-        {
-            return "You tried to use a numeric input in a text only field. This fired an error!";
-        }
+        public override string UEMessage() => "You tried to use a numeric input in a text only field. This fired an error!";
     }
 
     public class TextInputError : UserError
     {
-        public override string UEMessage()
-        {
-            return "You tried to use a text input in a numeric only field. This fired an error!";
-        }
+        public override string UEMessage() => "You tried to use a text input in a numeric only field. This fired an error!";
     }
 
     public class EmptyInputError : UserError
     {
-        public override string UEMessage()
-        {
-            return "You tried to input an empty value in a field that needed a value. This fired an error!";
-        }
+        public override string UEMessage() => "You tried to input an empty value in a field that needed a value. This fired an error!";
     }
 
     public class FloatInputError : UserError
     {
-        public override string UEMessage()
-        {
-            return "You tried to use a floating point value in an integer only field. This fired an error!";
-        }
+        public override string UEMessage() => "You tried to use a floating point value in an integer only field. This fired an error!";
     }
 
     public class NonBoolInputError : UserError
     {
-        public override string UEMessage()
-        {
-            return "You tried to input a non-boolean value in a boolean only field. This fired an error!";
-        }
+        public override string UEMessage() => "You tried to input a non-boolean value in a boolean only field. This fired an error!";
     }
 }
